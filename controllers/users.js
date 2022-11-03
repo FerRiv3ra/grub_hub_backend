b435efcd50ff82ce448f995484aab188920f5c7d
FerRivera <FerRivera2508@gmail.com>
2022-11-03 13:32:38 +0000

    Pagination removed from users

--- controllers/users.js
+++ controllers/users.js
@@ -3,14 +3,11 @@ const { response } = require('express');
 const User = require('../models/user');
 
 const getUsers = async (req, res = response) => {
-  const { limit = 5, from = 0 } = req.query;
-  if (Number(limit) === isNaN) limit = 5;
-  if (Number(from) === isNaN) from = 0;
   const query = { state: true };
 
   const [total, users] = await Promise.all([
     User.countDocuments(query),
-    User.find(query).skip(Number(from)).limit(Number(limit)),
+    User.find(query),
   ]);
 
   res.json({
